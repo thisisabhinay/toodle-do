fd36a763f21281e2057f60add8239dd2373a9d06
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-08-14 00:38:53 +0530

    build: create and use tailwind-config package

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -3,7 +3,9 @@ import * as React from "react"
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, React with Webpack, TypeScript, and JavaScript!</h1>
+      <h1 className="text-3xl font-semibold text-red-500">
+        Hello, React with Webpack, TypeScript, and JavaScript!
+      </h1>
     </div>
   )
 }
--- apps/shell/src/bootstrap.tsx
+++ apps/shell/src/bootstrap.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
+import "./index.css"
 import App from "./App"
 
 // @ts-ignore

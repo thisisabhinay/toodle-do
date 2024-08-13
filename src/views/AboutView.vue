1f226364531026a749ef6967fe5e48bd7129b674
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-08-13 22:41:59 +0530

    build: remove vite project

--- src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main>
-    <h1 class="text-3xl font-bold">About View</h1>
-  </main>
-</template>
--- src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main>
-    <h1 class="text-3xl font-bold">Home View</h1>
-  </main>
-</template>
